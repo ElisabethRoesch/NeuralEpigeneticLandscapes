19f5a4a48f31181118a0480f261370a9756497c7
ElisabethRoesch <eroesch@student.unimelb.edu.au>
2021-01-06 16:13:38 +1100

    running SDE multi IC, time.

--- 1_species_system/SDE/multi_IC/timecourse/SDE_multipe_IC_timecourse.jl
+++ 1_species_system/SDE/multi_IC/timecourse/SDE_multipe_IC_timecourse.jl
@@ -1,25 +1,39 @@
 using DiffEqFlux, StochasticDiffEq, Flux, Optim, Plots, DiffEqBase.EnsembleAnalysis, Statistics
 
-u0s = [[2.0],...]
+u0s = [[-3.],[ -1.], [0.], [1.0], [3.0]]
 datasize = 30
 tspan = (0.0f0, 1.5f0)
 tsteps = range(tspan[1], tspan[2], length = datasize)
 
-function trueSDEfunc(dx, x, p, t)
-    alpha=5
-    dx[1] =alpha*x[1]-x[1]*x[1]*x[1]
+function run_super_pf_sde_one(u0)
+    function trueSDEfunc(dx, x, p, t)
+        alpha=5
+        dx[1] =alpha*x[1]-x[1]*x[1]*x[1]
+    end
+    mp = Float32[0.2]
+    function true_noise_func(dx, x, p, t)
+        dx .= mp.*x
+    end
+    prob_truesde = SDEProblem(trueSDEfunc, true_noise_func, u0, tspan)
+    ensemble_prob = EnsembleProblem(prob_truesde)
+    ensemble_sol = solve(ensemble_prob, SOSRI(), trajectories = 10000)
+    ensemble_sum = EnsembleSummary(ensemble_sol)
+    sde_data, sde_data_vars = Array.(timeseries_point_meanvar(ensemble_sol, tsteps))
+    return sde_data, sde_data_vars
 end
 
-mp = Float32[0.2]
-function true_noise_func(dx, x, p, t)
-    dx .= mp.*x
+function run_super_pf_sde_multi(u0s)
+    sde_datae = []
+    sde_datae_vars = []
+    for u0 in u0s
+        temp_sde_data, temp_sde_vars = run_super_pf_sde_one(u0)
+        push!(sde_datae, temp_sde_data)
+        push!(sde_datae_vars, temp_sde_vars)
+    end
+    return sde_datae, sde_datae_vars
 end
-prob_truesde = SDEProblem(trueSDEfunc, true_noise_func, u0, tspan)
-ensemble_prob = EnsembleProblem(prob_truesde)
-ensemble_sol = solve(ensemble_prob, SOSRI(), trajectories = 10000)
-ensemble_sum = EnsembleSummary(ensemble_sol)
 
-sde_data, sde_data_vars = Array.(timeseries_point_meanvar(ensemble_sol, tsteps))
+sde_datae, sde_datae_vars = run_super_pf_sde_multi(u0s)
 
 drift_dudt = FastChain(FastDense(1, 50, tanh),
                        FastDense(50, 50, tanh),
@@ -33,54 +47,49 @@ function predict_neuralsde(p)
   Array(neuralsde(u0, p))
 end
 
+# loss function. Caution, the return is only for first training pair (where x = u0[1])
 function loss_neuralsde(p; n = 100)
-    loss = 0.0
-    counter = 0
-    for u0 in u0s
-        samples = [Array(neuralsde(u0, p)) for i in 1:n]
+
+    counter = 1
+    samples_first = [Array(neuralsde(u0s[counter], p)) for i in 1:n]
+    means_first = reshape(mean.([[samples_first[i][j] for i in 1:length(samples_first)] for j in 1:length(samples_first[1])]), size(samples_first[1])...)
+    vars_first = reshape(var.([[samples_first[i][j] for i in 1:length(samples_first)] for j in 1:length(samples_first[1])]), size(samples_first[1])...)
+    loss =  sum(abs2, sde_datae[counter] - means_first) + sum(abs2, sde_datae_vars[counter] - vars_first)
+    for i in 2:length(u0s)
+        counter = counter + 1
+        temp_u0 = u0s[counter]
+        samples = [Array(neuralsde(temp_u0, p)) for i in 1:n]
         means = reshape(mean.([[samples[i][j] for i in 1:length(samples)]
                                                 for j in 1:length(samples[1])]),
                               size(samples[1])...)
         vars = reshape(var.([[samples[i][j] for i in 1:length(samples)]
                                               for j in 1:length(samples[1])]),
                               size(samples[1])...)
-        s = sum(abs2, sde_data[counter] - means) + sum(abs2, sde_data_vars[counter] - vars)
+        s = sum(abs2, sde_datae[counter] - means) + sum(abs2, sde_datae_vars[counter] - vars)
         loss = loss + s
     end
-
-  return loss,
-        reshape(mean.([[samples[i][j] for i in 1:length(samples)]
-                                          for j in 1:length(samples[1])]),
-                        size(samples[1])...),
-        reshape(var.([[samples[i][j] for i in 1:length(samples)]
-                                        for j in 1:length(samples[1])]),
-                                              size(samples[1])...)
+    return loss,  means_first, vars_first
 end
 
 iter = 0
 
 callback = function (p, loss, means, vars; doplot = true)
-  global list_plots, iter
+    global list_plots, iter
 
-  if iter == 0
-    list_plots = []
-  end
-  iter += 1
-
-  # loss against current data
-  display(loss)
-
-  # plot current prediction against data
-  plt = scatter(tsteps, sde_data[1,:], yerror = sde_data_vars[1,:],
-                ylim = (1.0, 3.0),
+    if iter == 0
+        list_plots = []
+    end
+    iter += 1
+    display(loss)
+    plt = scatter(tsteps, sde_datae[1][1,:], yerror = sde_datae_vars[1][1,:],
+                ylim = (-3.0, -1.0),
                 label = "data")
-  scatter!(plt, tsteps, means[1,:], ribbon = vars[1,:], label = "prediction")
-  push!(list_plots, plt)
-
-  if doplot
-    display(plt)
-  end
-  return false
+        scatter!(plt, tsteps, means[1,:], ribbon = vars[1,:], label = "prediction")
+        push!(list_plots, plt)
+    if doplot
+        display(plt)
+    end
+    return false
 end
 
 
